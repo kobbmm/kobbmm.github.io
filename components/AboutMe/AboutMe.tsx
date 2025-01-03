1517e94fd477ce2f388944942bb15a86ae9fb01f
kobbkabb <a869370337@gmail.com>
2025-01-03 15:24:05 +0700

    Fix

--- components/AboutMe/AboutMe.tsx
+++ components/AboutMe/AboutMe.tsx
@@ -33,7 +33,7 @@ export default function AboutMe() {
         <div className="space-y-8 lg-grid-area-engineering">
         <h2 className="text-6xl font-bold text-white"></h2>
         <p className=" text-lg leading-relaxed text-white">
-        When it comes to crafting JavaScript applications, I work with a toolkit that’s just right for the job, yet I’m fully capable of stepping outside the box to solve
+        When it comes to crafting JavaScript applications, I work with a toolkit that's just right for the job, yet I'm fully capable of stepping outside the box to solve
          problems efficiently. I focus on delivering fast, reliable, and scalable solutions 
          that are built to grow with your needs. Performance and scalability are always top of mind,
           ensuring that every line of code I write is designed for resilience and long-term success.
