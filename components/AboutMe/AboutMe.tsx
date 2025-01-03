1269e8261f577f057c9a1dee2e74e2f920a86851
kobbkabb <a869370337@gmail.com>
2025-01-03 15:28:29 +0700

    F

--- components/AboutMe/AboutMe.tsx
+++ components/AboutMe/AboutMe.tsx
@@ -23,20 +23,20 @@ export default function AboutMe() {
         <h1 className='text-6xl text-white font-bold '  data-aos="slide-right" >AboutMe</h1>
         
         <p className="text-linear text-lg leading-relaxed text-white">
-        "I am a passionate and detail-oriented Software Engineering student
+        I am a passionate and detail-oriented Software Engineering student
         specializing in Frontend Development. With hands-on experience in
         building responsive websites and web applications, I am proficient in HTML,
         CSS, JavaScript, and modern frontend frameworks like React.js. I am committed
-        to creating user-friendly, visually appealing interfaces that deliver seamless user experiences."
+        to creating user-friendly, visually appealing interfaces that deliver seamless user experiences.
         </p>
         </div>
         <div className="space-y-8 lg-grid-area-engineering">
         <h2 className="text-6xl font-bold text-white"></h2>
         <p className=" text-lg leading-relaxed text-white">
         When it comes to crafting JavaScript applications, I work with a toolkit that's just right for the job, yet I'm fully capable of stepping outside the box to solve
-         problems efficiently. I focus on delivering fast, reliable, and scalable solutions 
-         that are built to grow with your needs. Performance and scalability are always top of mind,
-          ensuring that every line of code I write is designed for resilience and long-term success.
+        problems efficiently. I focus on delivering fast, reliable, and scalable solutions 
+        that are built to grow with your needs. Performance and scalability are always top of mind,
+        ensuring that every line of code I write is designed for resilience and long-term success.
         </p>
         </div>
         </div>
