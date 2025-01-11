74cd88d5c06c6306c7ef13f81e4c4e9eac14766c
kobbkabb <a869370337@gmail.com>
2025-01-11 16:13:40 +0700

    Revert "REvert-v"
    This reverts commit cf6bbcaabd196ffe6a5ace7197c98e85077f770d.

--- components/Skills/1st-Rows/Myskills.tsx
+++ components/Skills/1st-Rows/Myskills.tsx
@@ -5,14 +5,13 @@ import "./Me.css";
 interface ZigzagBlockProps {
   imageSrc: string;
   altText: string;
-  width: number;
-  height: number;
+ 
 }
 
-const ZigzagBlock: React.FC<ZigzagBlockProps> = ({ imageSrc, altText, width, height }) => {
+const ZigzagBlock: React.FC<ZigzagBlockProps> = ({ imageSrc, altText }) => {
   return (
     <div className="zigzag-block">
-      <Image src={imageSrc} alt={altText} width={width} height={height} className="zigzag-image" layout="responsive" />
+      <Image src={imageSrc} alt={altText} width={width} height={height} className="zigzag-image" />
     </div>
   );
 };
@@ -20,48 +19,36 @@ const ZigzagBlock: React.FC<ZigzagBlockProps> = ({ imageSrc, altText, width, hei
 const Zigzag: React.FC = () => {
   return (
     <div id="Skills">  
-        <h1 className="top-left-title">Skill</h1>    
+        <h1 className="top-left-title ">Skill</h1>    
         <div className="zigzag-container">
           {/* บล็อกที่ 1 */}
           <ZigzagBlock
             imageSrc="/src/html5.svg"
-            altText="HTML5 Image"
-            width={100}
-            height={100}
+            altText="Java Image"
           />
           {/* บล็อกที่ 2 */}
           <ZigzagBlock
             imageSrc="/src/css3.svg"
-            altText="CSS3 Image"
-            width={100}
-            height={100}
+            altText="JavaScript Image"
           />
           <ZigzagBlock
             imageSrc="/src/java.png"
-            altText="Java Image"
-            width={100}
-            height={100}
+            altText="Python Image"
           />
           {/* บล็อกที่ 3 */} 
           <ZigzagBlock
             imageSrc="/src/react.svg"
-            altText="React Image"
-            width={100}
-            height={100}
+            altText="Python Image"
           />
           {/* บล็อกที่ 3 */}     
           <ZigzagBlock
             imageSrc="/src/tailwind.svg"
-            altText="Tailwind Image"
-            width={100}
-            height={100}
+            altText="Python Image"
           />
           {/* บล็อกที่ 3 */}     
           <ZigzagBlock
             imageSrc="/src/javascript.svg"
-            altText="JavaScript Image"
-            width={100}
-            height={100}
+            altText="Python Image"
           />
           {/* บล็อกที่ 3 */}            
         </div>
